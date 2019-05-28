ae4c5e23caf5f00f77dd727aaa8a58749d323a84
NCJ <iamncj@outlook.com>
2019-05-28 18:52:34 +0800

    file

---
+++ File_System/debug/file_debug.c
@@ -0,0 +1,13 @@
+//
+// Created by Tao Chiang on 5/28/2019.
+//
+#include "file_system.h"
+
+int main()
+{
+    userNode * rank_list;
+    rank_list = Load_Rank ();
+    rank_list = Add_Node (rank_list, 250, "66666");
+    write_Rank (rank_list);
+    return 0;
+}
