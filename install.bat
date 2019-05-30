e51f45f32c1c71ec2be2f242c6a431629d4809cd
NCJ <iamncj@outlook.com>
2019-05-30 19:13:21 +0800

    new bat

--- install.bat
+++ install.bat
@@ -1,1 +1,42 @@
-make
+@echo off
+:: make
+mingw32-make
+
+:: move to bin
+mkdir bin
+for %%n in (*.exe) do (
+	move "%%n" bin\
+)
+
+:: shortcut
+::设置程序或文件的完整路径（必选）
+set Program=C:\tetri_game\bin\launcher.exe
+ 
+::设置快捷方式名称（必选）
+set LnkName=Tetris
+ 
+::设置程序的工作路径，一般为程序主目录，此项若留空，脚本将自行分析路径
+set WorkDir=
+ 
+::设置快捷方式显示的说明（可选）
+::set Desc=测试
+ 
+if not defined WorkDir call:GetWorkDir "%Program%"
+(echo Set WshShell=CreateObject("WScript.Shell"^)
+echo strDesKtop=WshShell.SpecialFolders("DesKtop"^)
+echo Set oShellLink=WshShell.CreateShortcut(strDesKtop^&"\%LnkName%.lnk"^)
+echo oShellLink.TargetPath="%Program%"
+echo oShellLink.WorkingDirectory="%WorkDir%"
+echo oShellLink.WindowStyle=1
+echo oShellLink.Description="%Desc%"
+echo oShellLink.IconLocation="C:\tetri_game\tetri.ico"
+echo oShellLink.Save)>makelnk.vbs
+:: echo 桌面快捷方式创建成功！
+makelnk.vbs
+del /f /q makelnk.vbs
+exit
+goto :eof
+:GetWorkDir
+set WorkDir=%~dp1
+set WorkDir=%WorkDir:~,-1%
+goto :eof
