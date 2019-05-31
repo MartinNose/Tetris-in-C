bc120bd1719852ada8d896d38f2552044798e0d2
MartinNose <liucmartin@qq.com>
2019-05-31 23:19:15 +0800

    added comments in handler.h

--- single_src/controller/handlers.h
+++ single_src/controller/handlers.h
@@ -4,8 +4,29 @@
 
 #ifndef _HANDLERS_H_
 #define _HANDLERS_H_
-
+/*
+ *  Function: mouseEventHandler
+ *  Usage: mouseEventHandler (x,y,button,event)
+ *  ---------------------------------------
+ *  This function can handle the mouse event. 
+ * */
 void mouseEventHandler (int x, int y, int button, int event);
+
+/*
+ *  Function: keyboardEventHandler
+ *  Usage: keyboardEventHandler(VK_DOWN,KEY_UP)
+ *  ---------------------------------------
+ *  This function can handle the keyboard event.
+ * */
+
 void keyboardEventHandler (int key, int event);
+
+/*
+ *  Function: CharEventHandler
+ *  Usage: CharEventHandler(char c);
+ *  ---------------------------------------
+ *  This function can handle the char input event.
+ * */
+
 void CharEventHandler(char c);
 #endif //_HANDLERS_H_
