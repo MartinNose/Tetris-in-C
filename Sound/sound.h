0d3ff9fcaf6d3a07c882be8dbe8b7be480b1b43f
NCJ <iamncj@outlook.com>
2019-05-31 14:52:04 +0800

    comments on sound

--- Sound/sound.h
+++ Sound/sound.h
@@ -3,8 +3,21 @@
 
 #include<windows.h>
 
+/*
+ * Function: Play_Sound_Demo
+ * Usage: Call it to make sound effects
+ * ====================================
+ * It runs in a new thread to avoid UI stuck
+ */
 void Play_Sound_Demo ();
 
-void Play_Sound();
+/*
+ * Function: Play_Sound
+ * Usage: Call it to play music
+ * ============================
+ * It plays wave sound and it
+ * also runs in a new thread
+ */
+void Play_Sound ();
 
 #endif //_SOUND_H_
